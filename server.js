c632cd534b4423af6e5da982b7d70af8f5cf0073
w4tsw4ts <billy.montgomery@ge.com>
2018-11-23 22:34:32 -0500

    Update server.js with require express and body-parser

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 // Require Express and Body Parser
 let express = require("express");
 let bodyParser = require("body-parser");
+
+// ==============================================================================
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+// Example routes
+// require("./app/routes/apiRoutes")(app);
+// require("./app/routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
