adfa8c1939f4274d2a3049d489955d6016f84692
w4tsw4ts <billy.montgomery@ge.com>
2018-11-24 13:27:19 -0500

    Update burger.js file to call ORM

---
+++ models/burger.js
@@ -0,0 +1,24 @@
+// Import the ORM to create function that will interact with the database.
+const orm = require("../config/orm.js");
+
+const burger = {
+    selectAll: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
+    // The variables cols and vals are arrays.
+    createOne: function(cols, vals, cb) {
+        orm.create("burgers", cols, vals, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.update("burgers", objColVals, condition, function(res) {
+            cb(res);
+        });
+    }
+};
+
+// Export the database functions for the controller (catsController.js).
+module.exports = burger;
